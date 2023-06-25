49afeb913eee4f3049021b5507886f66e6a96bef
Midhun T <Midhun.T@ibm.com>
2023-06-25 18:19:13 +0530

    bash to ansible change 18

--- azure/scripts/bootstrap.sh
+++ azure/scripts/bootstrap.sh
@@ -158,12 +158,12 @@ runuser -l $BOOTSTRAP_ADMIN_USERNAME -c "git clone --branch bash-ansible https:/
 echo $(date) " - Setup Install config"
 runuser -l $BOOTSTRAP_ADMIN_USERNAME -c "mkdir -p $INSTALLERHOME/openshiftfourx"
 runuser -l $BOOTSTRAP_ADMIN_USERNAME -c "touch $INSTALLERHOME/openshiftfourx/install-config.yaml"
-export zones=""
+zones=""
 if [[ $SINGLE_ZONE_OR_MULTI_ZONE == "az" ]]; then
-export zones="zones:
-                - '1'
-                - '2'
-                - '3'"
+zones="zones:
+      - '1'
+      - '2'
+      - '3'"
 fi
 runuser -l $BOOTSTRAP_ADMIN_USERNAME -c "cp $INSTALLERHOME/experiments/azure/scripts/install-config.yaml $INSTALLERHOME/openshiftfourx/install-config.yaml"
 echo $(date) " - Setup Install config - Complete"
