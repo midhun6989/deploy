fc4d3663b02493f881a6f8dc9c048db2007527f4
Midhun T <Midhun.T@ibm.com>
2023-06-29 00:20:03 +0530

    ansible playbook 10

--- azure/scripts/bootstrap.sh
+++ azure/scripts/bootstrap.sh
@@ -6,6 +6,14 @@ mkdir -p $INSTALLER_HOME/experiments
 export DEBUG_LOG=$INSTALLER_HOME/debug.log
 touch $DEBUG_LOG
 
+echo $(date) " - Install git - Start" >> $DEBUG_LOG
+sudo dnf install -y git
+echo $(date) " - Install git - Complete" >> $DEBUG_LOG
+
+echo $(date) " - Clone GitHub Repository - Start" >> $DEBUG_LOG
+git clone --branch ansible-playbook https://github.com/midhun6989/experiments.git $INSTALLER_HOME/experiments
+echo $(date) " - Clone GitHub Repository - Complete" >> $DEBUG_LOG
+
 echo $(date) " - Install Ansible - Start" >> $DEBUG_LOG
 sudo dnf install -y python3-pip
 sudo pip3 install --upgrade pip
